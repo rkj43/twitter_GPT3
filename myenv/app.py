01e6425df108d01f6a986caba849e9c2f88f3362
Rahul <rahulkjohn43@gmail.com>
2023-01-31 18:06:59 +0000

    Updated to also list the tweets

--- myenv/app.py
+++ myenv/app.py
@@ -23,18 +23,22 @@ def index():
 
 @app.route("/sentiment", methods=["POST"])
 def sentiment():
-    # Get the hashtag from the form
+    # Get the hashtag from the form index.html
     hashtag = request.form["hashtag"]
 
      # Retrieve the latest 200 tweets for the given hashtag
-    tweets = api.search_tweets(q="#" + hashtag, count=200)
+    tweets = api.search_tweets(q="#" + hashtag + '-is:retweet lang:en', count=200)
+    #-is:retweet means I don't want retweets
+    # lang:en is asking for the tweets to be in english
 
     # Perform sentiment analysis on each tweet
     sentiments = []
+    tweet_sentiments = []
     for tweet in tweets:
         analysis = TextBlob(tweet.text)
         sentiment = analysis.sentiment.polarity
         sentiments.append(sentiment)
+        tweet_sentiments.append((tweet.text, sentiment))
 
     # Determine the overall sentiment for the tweets
     overall_sentiment = sum(sentiments) / len(sentiments)
@@ -46,7 +50,7 @@ def sentiment():
         sentiment = "neutral"
 
     # Render the result in the template
-    return render_template("sentiment.html", sentiment=sentiment, hashtag="#" + hashtag)
+    return render_template("sentiment.html", sentiment=sentiment, hashtag="#" + hashtag, tweet_sentiments=tweet_sentiments)
 
 
 if __name__ == "__main__":
